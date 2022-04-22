f956758466f31d3640daf81cae8021871d2c2d3e
achilleswargod <65618715+achilleswargod@users.noreply.github.com>
2022-04-22 14:56:20 -0500

    "Completed Functionality for problem 1"

---
+++ DatabaseFirstLINQ/DatabaseFirstLINQ/Problems.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using DatabaseFirstLINQ.Models;
+using System.Collections.Generic;
+
+namespace DatabaseFirstLINQ
+{
+    class Problems
+    {
+        private ECommerceContext _context;
+
+        public Problems()
+        {
+            _context = new ECommerceContext();
+        }
+        public void RunLINQQueries()
+        {
+            ProblemOne();
+            //ProblemTwo();
+            //ProblemThree();
+            //ProblemFour();
+            //ProblemFive();
+            //ProblemSix();
+            //ProblemSeven();
+            //ProblemEight();
+            //ProblemNine();
+            //ProblemTen();
+            //ProblemEleven();
+            //ProblemTwelve();
+            //ProblemThirteen();
+            //ProblemFourteen();
+            //ProblemFifteen();
+            //ProblemSixteen();
+            //ProblemSeventeen();
+            //ProblemEighteen();
+            //ProblemNineteen();
+            //ProblemTwenty();
+        }
+
+        // <><><><><><><><> R Actions (Read) <><><><><><><><><>
+        private void ProblemOne()
+        {
+            // Write a LINQ query that returns the number of users in the Users table.
+            // HINT: .ToList().Count
+
+            List<User> usersCounted = _context.Users.ToList();
+
+ 
+            Console.WriteLine(usersCounted.Count());
+            
+        }
+
+        private void ProblemTwo()
+        {
+            // Write a LINQ query that retrieves the users from the User tables then print each user's email to the console.
+            var users = _context.Users;
+
+            foreach (User user in users)
+            {
+                Console.WriteLine(user.Email);
+            }
+
+        }
+
+        private void ProblemThree()
+        {
+            // Write a LINQ query that gets each product where the products price is greater than $150.
+            // Then print the name and price of each product from the above query to the console.
+
+        }
+
+        private void ProblemFour()
+        {
+            // Write a LINQ query that gets each product that contains an "s" in the products name.
+            // Then print the name of each product from the above query to the console.
+
+        }
+
+        private void ProblemFive()
+        {
+            // Write a LINQ query that gets all of the users who registered BEFORE 2016
+            // Then print each user's email and registration date to the console.
+
+        }
+
+        private void ProblemSix()
+        {
+            // Write a LINQ query that gets all of the users who registered AFTER 2016 and BEFORE 2018
+            // Then print each user's email and registration date to the console.
+
+        }
+
+        // <><><><><><><><> R Actions (Read) with Foreign Keys <><><><><><><><><>
+
+        private void ProblemSeven()
+        {
+            // Write a LINQ query that retreives all of the users who are assigned to the role of Customer.
+            // Then print the users email and role name to the console.
+            var customerUsers = _context.UserRoles.Include(ur => ur.Role).Include(ur => ur.User).Where(ur => ur.Role.RoleName == "Customer");
+            foreach (UserRole userRole in customerUsers)
+            {
+                Console.WriteLine($"Email: {userRole.User.Email} Role: {userRole.Role.RoleName}");
+            }
+        }
+
+        private void ProblemEight()
+        {
+            // Write a LINQ query that retreives all of the products in the shopping cart of the user who has the email "afton@gmail.com".
+            // Then print the product's name, price, and quantity to the console.
+
+        }
+
+        private void ProblemNine()
+        {
+            // Write a LINQ query that retreives all of the products in the shopping cart of the user who has the email "oda@gmail.com" and returns the sum of all of the products prices.
+            // HINT: End of query will be: .Select(sc => sc.Product.Price).Sum();
+            // Then print the total of the shopping cart to the console.
+
+        }
+
+        private void ProblemTen()
+        {
+            // Write a LINQ query that retreives all of the products in the shopping cart of users who have the role of "Employee".
+            // Then print the user's email as well as the product's name, price, and quantity to the console.
+
+        }
+
+        // <><><><><><><><> CUD (Create, Update, Delete) Actions <><><><><><><><><>
+
+        // <><> C Actions (Create) <><>
+
+        private void ProblemEleven()
+        {
+            // Create a new User object and add that user to the Users table using LINQ.
+            User newUser = new User()
+            {
+                Email = "david@gmail.com",
+                Password = "DavidsPass123"
+            };
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+        }
+
+        private void ProblemTwelve()
+        {
+            // Create a new Product object and add that product to the Products table using LINQ.
+
+        }
+
+        private void ProblemThirteen()
+        {
+            // Add the role of "Customer" to the user we just created in the UserRoles junction table using LINQ.
+            var roleId = _context.Roles.Where(r => r.RoleName == "Customer").Select(r => r.Id).SingleOrDefault();
+            var userId = _context.Users.Where(u => u.Email == "david@gmail.com").Select(u => u.Id).SingleOrDefault();
+            UserRole newUserRole = new UserRole()
+            {
+                UserId = userId,
+                RoleId = roleId
+            };
+            _context.UserRoles.Add(newUserRole);
+            _context.SaveChanges();
+        }
+
+        private void ProblemFourteen()
+        {
+            // Add the product you create to the user we created in the ShoppingCart junction table using LINQ.
+
+        }
+
+        // <><> U Actions (Update) <><>
+
+        private void ProblemFifteen()
+        {
+            // Update the email of the user we created to "mike@gmail.com"
+            var user = _context.Users.Where(u => u.Email == "david@gmail.com").SingleOrDefault();
+            user.Email = "mike@gmail.com";
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+
+        private void ProblemSixteen()
+        {
+            // Update the price of the product you created to something different using LINQ.
+
+        }
+
+        private void ProblemSeventeen()
+        {
+            // Change the role of the user we created to "Employee"
+            // HINT: You need to delete the existing role relationship and then create a new UserRole object and add it to the UserRoles table
+            // See problem eighteen as an example of removing a role relationship
+            var userRole = _context.UserRoles.Where(ur => ur.User.Email == "mike@gmail.com").SingleOrDefault();
+            _context.UserRoles.Remove(userRole);
+            UserRole newUserRole = new UserRole()
+            {
+                UserId = _context.Users.Where(u => u.Email == "mike@gmail.com").Select(u => u.Id).SingleOrDefault(),
+                RoleId = _context.Roles.Where(r => r.RoleName == "Employee").Select(r => r.Id).SingleOrDefault()
+            };
+            _context.UserRoles.Add(newUserRole);
+            _context.SaveChanges();
+        }
+    }
+}
