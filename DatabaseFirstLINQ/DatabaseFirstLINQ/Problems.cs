434e407949ededddcfb634a8cb2d9450ff103049
urivat <rashad.porter18@gmail.com>
2022-04-25 17:01:36 -0400

    bonusOne

--- DatabaseFirstLINQ/DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/DatabaseFirstLINQ/Problems.cs
@@ -307,6 +307,16 @@ namespace DatabaseFirstLINQ
             // Prompt the user to enter in an email and password through the console.
             // Take the email and password and check if the there is a person that matches that combination.
             // Print "Signed In!" to the console if they exists and the values match otherwise print "Invalid Email or Password.".
+           
+            Console.WriteLine("Enter your Email:\n Enter your password: ");
+            string response = Console.ReadLine(); 
+            var user = _context.Users.Where(u => u.Email  && u.Password == response);
+            
+
+            
+
+
+
         }
 
         private void BonusTwo()
